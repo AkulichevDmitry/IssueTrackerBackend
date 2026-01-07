7597afd48c501f9638968cb6692482b58a851ecc
Dmitry <118440313+AkulichevDmitry@users.noreply.github.com>
2026-01-07 17:23:24 +0300

    feat(internal/domain/entities/user.go): add email field to user entity

--- internal/domain/entities/user.go
+++ internal/domain/entities/user.go
@@ -5,11 +5,13 @@ import (
 )
 
 type UserID int64
+type UserEmail string
 
 type UserEntity struct {
 	ID        UserID
 	CompanyID CompanyID
 	Username  string
+	Email     UserEmail
 	Password  string
 	Rating    int64
 	IsCompany bool
